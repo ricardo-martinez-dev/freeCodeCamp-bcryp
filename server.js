ca58c0f0568bbd04a99fd34a31db7c15c821f338
Ricardo <ricardo@Ricardos-MacBook-Pro.local>
2025-11-03 17:08:51 +0100

    update

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-"use strict";
 const express = require("express");
 const bodyParser = require("body-parser");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const bcrypt = require("bcrypt");
+("use strict");
 const app = express();
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = "sUperpassw0rd!";
 const someOtherPlaintextPassword = "pass123";
-
-app.get("/", (req, res) => {
-  res.send("Hello freeCodeCamp bcrypt");
-});
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
